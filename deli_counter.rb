753ef2f89f39b62ce03f1f6832526ea7132b82bd
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-22 04:44:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@ def now_serving(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  full_line.push(name)
+  katz_deli.push(name)
   puts "Welcome #{name}. You are number #{katz_deli.length -1} in line."
 end
