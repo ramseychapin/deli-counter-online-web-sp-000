54a9af3ce2715655e99c2dd73ebeba05974752d7
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-22 04:38:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,9 +10,8 @@ def line(katz_deli)
     katz_deli.each.with_index do |name, index|
       line_array.push("#{index}. #{name}")
     end
-  puts "The line is currently: #{line_array.join(" ")}"
   end
-
+    puts "The line is currently: #{line_array.join(" ")}"
 end
 
 
