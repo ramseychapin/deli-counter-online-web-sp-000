3755a1425160217504c1c18915c9281a41c31444
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-22 04:40:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 
 def line(katz_deli)
   if (katz_deli.length == 0)
-    puts "The line is currently empty."
+    "The line is currently empty."
   else
     line_array = []
     katz_deli.each.with_index do |name, index|
