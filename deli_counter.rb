5339af2605d697c11b1ab4d10a2a08a1d12a4610
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-22 04:34:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def now_serving(katz_deli)
   puts "Currently serving #{katz_deli.shift}"
 end
 
-def takeANumber(katz_deli, name)
+def take_a_number(katz_deli, name)
   full_line.push(name)
   puts "Welcome #{name}. You are number #{katz_deli.length -1} in line."
 end
