5752158833b758919f4f958b95117840663fac5e
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-22 04:50:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 
 def now_serving(katz_deli)
-  if katz_deli.empty
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}"
