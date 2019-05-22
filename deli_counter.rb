bc2fcabe2fa681446785b7204328f0ad5e9f2ec6
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-22 04:02:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(full_line)
   if (full_line.length == 0)
     "The line is currently empty."
   else
-    
+
 end
 
 
@@ -14,5 +14,5 @@ def now_serving(full_line)
 end
 
 def take_a_number(full_line, name)
-
+  full_line.push(name)
 end
